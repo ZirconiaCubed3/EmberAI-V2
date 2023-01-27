797fbc8d13400a853bef2e99aeeaeff9731b5129
ZirconiaCubed3 <lucian.819643@nv.ccsd.net>
2023-01-26 19:39:44 -0500

    Added Continuous Training Support

---
+++ train-more.py
@@ -0,0 +1,172 @@
+import numpy as np
+import os
+import time
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow as tf
+import sys
+
+start = time.time()
+
+path_to_file = "words.txt"
+
+text = open(path_to_file, "rb").read().decode(encoding="utf-8")
+
+vocab = sorted(set(text))
+
+ids_from_chars = tf.keras.layers.StringLookup(vocabulary=list(vocab), mask_token=None)
+
+chars_from_ids = tf.keras.layers.StringLookup(vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=1)
+
+all_ids = ids_from_chars(tf.strings.unicode_split(text, "UTF-8"))
+
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+
+seq_length = 100
+
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(sequence):
+  input_text = sequence[:-1]
+  target_text = sequence[1:]
+  return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 10000
+
+dataset = (dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE))
+
+vocab_size = len(ids_from_chars.get_vocabulary())
+embedding_dim = 256
+rnn_units = 1024
+
+class MyModel(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, rnn_units):
+    super().__init__(self)
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(rnn_units, return_sequences=True, return_state=True)
+    self.dense = tf.keras.layers.Dense(vocab_size)
+
+  def call(self, inputs, states=None, return_state=False, training=False):
+    x = inputs
+    x = self.embedding(x, training=training)
+    if states is None:
+      states = self.gru.get_initial_state(x)
+    x, states = self.gru(x, initial_state=states, training=training)
+    x = self.dense(x, training=training)
+
+    if return_state:
+      return x, states
+    else:
+      return x
+
+class CustomTraining(MyModel):
+  @tf.function
+  def train_step(self, inputs):
+    inputs, labels = inputs
+    with tf.GradientTape() as tape:
+      predictions = self(inputs, training=True)
+      loss = self.loss(labels, predictions)
+    grads = tape.gradient(loss, model.trainable_variables)
+    self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+    return {"loss": loss}
+
+model = CustomTraining(vocab_size=vocab_size, embedding_dim=embedding_dim, rnn_units=rnn_units)
+
+
+for input_example_batch, target_example_batch in dataset.take(1):
+  example_batch_predictions = model(input_example_batch)
+
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+
+
+loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+example_batch_mean_loss = loss(target_example_batch, example_batch_predictions)
+
+model.compile(optimizer="adam", loss=loss)
+
+checkpoint_dir = "./training_checkpoints"
+
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix, save_weights_only=True)
+
+if __name__ == "__main__":
+  if sys.argv[1] == "train":
+    EPOCHS = int(sys.argv[2])
+    try:
+      model = tf.saved_model.load("actual_model")
+    except Exception as e:
+      print("Could not find a saved model. Training from scratch.")
+    history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+    tf.saved_model.save(model, "actual_model")
+
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
+
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+    predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                          return_state=True)
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    predicted_logits = predicted_logits + self.prediction_mask
+
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+    predicted_chars = self.chars_from_ids(predicted_ids)
+
+    return predicted_chars, states
+
+if __name__ == "__main__":
+  if sys.argv[1] == "load":
+    one_step_model = tf.saved_model.load("one_step")
+  elif sys.argv[1] == "train":
+    one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+else:
+  one_step_model = tf.saved_model.load("one_step")
+
+def predict(length):
+  states = None
+  next_char = tf.constant(["I"])
+  result = [next_char]
+
+  for n in range(length):
+    next_char, states = one_step_model.generate_one_step(next_char, states=states)
+    result.append(next_char)
+
+  result = tf.strings.join(result)
+  #end = time.time()
+  #print(result[0].numpy().decode("utf-8"), "\n\n" + "_"*80)
+  #print("\nRun Time:", end - start)
+  #if sys.argv[1] == "train":
+  #  tf.saved_model.save(one_step_model, "one_step")
+  return result[0].numpy().decode("utf-8")
+
+if __name__ == "__main__":
+  if sys.argv[1] == "train":
+    tf.saved_model.save(one_step_model, "one_step")
+    predict(50)
